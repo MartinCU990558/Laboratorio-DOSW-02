41badbc2865c2a4d1f70a464b6892275113fd32e
komi_yes <daniel.palacios-m@mail.escuelaing.edu.co>
2025-08-15 18:34:05 -0500

    feature: creacion del meodo main y la clase mensaje

--- Pareja_Palacios_Salamanca/reto1/Reto1.java
+++ Pareja_Palacios_Salamanca/reto1/Reto1.java
@@ -1,8 +1,8 @@
 public class Reto1{
 
     public static void main (String[] args){
-        Estudiante daniel = new Estudiante("Daniel Palacios", 20, "daniel.palacios-m@mail.escuelaing.edu.co", 6){
+        Estudiante daniel = new Estudiante("Daniel Palacios", 20, "daniel.palacios-m@mail.escuelaing.edu.co", 6);
+        Estudiante david = new Estudiante("David Salamanca", 20, "david.salamanca-a@mail.escuelaing.edu.co", 7);
 
-        }
     }
 }
