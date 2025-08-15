328ec4770509bba149a01f60badc2b43d34fce53
Sofia Ariza <sofia.ariza-g@mail.escuelaing.edu.co>
2025-08-15 18:31:43 -0500

    reto 1 fixes

--- Pareja7_Ramirez_Ariza/reto_1_ramirez_ariza/src/reto1/Greeting_message.java
+++ Pareja7_Ramirez_Ariza/reto_1_ramirez_ariza/src/reto1/Greeting_message.java
@@ -9,7 +9,7 @@ public class Greeting_message {
     public static void print_message(ArrayList<Student> students) {
         System.out.println("¡Hola bienvenidos! Nosotros somos la pareja conformada por " +
                 students.stream().map(n -> n.getName() +
-                        " de la escuela de " + n.getSemester() + " semestre de " + n.getAge()+ " años").collect(Collectors.joining(" y ")) +
+                " estudiante de la escuela de " + n.getSemester() + " semestre de " + n.getAge()+ " años").collect(Collectors.joining(" y ")) +
                 ",\n nuestros correos institucionales son "+
                 students.stream().map(Student::getEmail).collect(Collectors.joining(" y ")));
     }
