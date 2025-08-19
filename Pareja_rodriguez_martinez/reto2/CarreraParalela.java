79f4620602981921f767eaa10e28ababbe1289e7
NikoMAR3 <nikolas.martinezrive@mail.escuelaing.edu.co>
2025-08-18 22:55:53 -0500

    Implementacion metodo minimo de una lista

--- Pareja_rodriguez_martinez/reto2/CarreraParalela.java
+++ Pareja_rodriguez_martinez/reto2/CarreraParalela.java
@@ -1,5 +1,11 @@
 package Pareja_rodriguez_martinez.reto2;
 
+import java.util.List;
+
 public class CarreraParalela {
-    
+
+    public int findMinimum(List<Integer> listOfIntegers ){
+        return listOfIntegers.stream().min(Integer::compare).get();
+    }
+
 }
