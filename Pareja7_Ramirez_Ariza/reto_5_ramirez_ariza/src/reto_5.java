ba564b4f0760ff39e65887f9d65c562c38dc4871
Tomas Ramirez <tomas.ramirez@mail.escuelaing.edu.co>
2025-08-20 11:42:32 -0500

    reto 5 parte 2

--- Pareja7_Ramirez_Ariza/reto_5_ramirez_ariza/src/reto_5.java
+++ Pareja7_Ramirez_Ariza/reto_5_ramirez_ariza/src/reto_5.java
@@ -1,11 +1,18 @@
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class reto_5 {
     public static Set<Integer> filterMultiplesOfThree(List<Integer> numeros) {
         Set<Integer> set = new HashSet<>(numeros);
         set.removeIf(n -> n % 3 == 0);
+        return set;
+    }
+
+    public static Set<Integer> filterMultiplesOfFive(List<Integer> numeros) {
+        Set<Integer> set = new TreeSet<>(numeros);
+        set.removeIf(n -> n % 5 == 0);
         return set;
     }
 
@@ -15,6 +22,7 @@ public class reto_5 {
         List<Integer> listaTreeSet = List.of(12, 3, 25, 10, 7, 30, 18, 4);
 
         System.out.println(filterMultiplesOfThree(listaHashSet));
+        System.out.println(filterMultiplesOfFive(listaHashSet));
     }
 
 }
