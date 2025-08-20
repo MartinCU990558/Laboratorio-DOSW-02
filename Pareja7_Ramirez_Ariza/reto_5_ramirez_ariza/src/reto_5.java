8513e2a560b57d2fe120848eca61c98e5ad49478
Sofia <Sofia>
2025-08-20 11:37:39 -0500

    Implemented filterMultiplesOfThree function for challenge 5

--- Pareja7_Ramirez_Ariza/reto_5_ramirez_ariza/src/reto_5.java
+++ Pareja7_Ramirez_Ariza/reto_5_ramirez_ariza/src/reto_5.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class reto_5 {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class reto_5 {
+    public static Set<Integer> filterMultiplesOfThree(List<Integer> numeros) {
+        Set<Integer> set = new HashSet<>(numeros);
+        set.removeIf(n -> n % 3 == 0);
+        return set;
     }
+
+
+    public static void main(String[] args) {
+        List<Integer> listaHashSet = List.of(4, 9, 15, 7, 18, 21, 10, 5);
+        List<Integer> listaTreeSet = List.of(12, 3, 25, 10, 7, 30, 18, 4);
+
+        System.out.println(filterMultiplesOfThree(listaHashSet));
+    }
+
 }
