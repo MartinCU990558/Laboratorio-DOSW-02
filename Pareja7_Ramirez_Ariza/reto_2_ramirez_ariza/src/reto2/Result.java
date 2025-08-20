850fb5e6b4b5cb01a53ea736ee8a0549a6a087f6
Sofia <Sofia>
2025-08-19 23:31:53 -0500

    lambda functions aplication in toString in result for better results

--- Pareja7_Ramirez_Ariza/reto_2_ramirez_ariza/src/reto2/Result.java
+++ Pareja7_Ramirez_Ariza/reto_2_ramirez_ariza/src/reto2/Result.java
@@ -3,40 +3,33 @@ package reto2;
 import java.util.ArrayList;
 
 public class Result {
-    private int max1;
-    private int min1;
-    private int count1;
-    private boolean isMax1MultipleOfTwo;
-    private boolean isMax1DivisorOfTwo;
-    private boolean isCount1Even;
-
-    private final int max2;
-    private final int min2;
-    private final int count2;
-    private final boolean isMax2MultipleOfTwo;
-    private boolean isMax2DivisorOfTwo;
-    private boolean isCount2Even;
+    private ArrayList<Object> list1Result = new ArrayList<>();
+    private ArrayList<Object> list2Result = new ArrayList<>();
 
     public Result(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        max1 = CarreraParalela.maximum(list1);
-        min1 = CarreraParalela.minunum(list1).get(0);
-        count1 = CarreraParalela.minunum(list1).get(1);
-        isMax1MultipleOfTwo = CarreraParalela.validateMultipleOfTwo(list1);
-        isMax1DivisorOfTwo = CarreraParalela.validateDivOfTwo(list1);
-        isCount1Even = CarreraParalela.validateEvenSize(list1);
+        list1Result.add(CarreraParalela.maximum(list1));
+        list1Result.add(CarreraParalela.minunum(list1).get(0));
+        list1Result.add(CarreraParalela.minunum(list1).get(1));
+        list1Result.add(CarreraParalela.validateMultipleOfTwo(list1));
+        list1Result.add(CarreraParalela.validateDivOfTwo(list1));
+        list1Result.add(CarreraParalela.validateEvenSize(list1));
 
-        max2 = CarreraParalela.maximum(list2);
-        min2 = CarreraParalela.minunum(list2).get(0);
-        count2 = CarreraParalela.minunum(list2).get(1);
-        isMax2MultipleOfTwo = CarreraParalela.validateMultipleOfTwo(list2);
-        isMax2DivisorOfTwo = CarreraParalela.validateDivOfTwo(list2);
-        isCount2Even = CarreraParalela.validateEvenSize(list2);
+        list2Result.add(CarreraParalela.maximum(list2));
+        list2Result.add(CarreraParalela.minunum(list2).get(0));
+        list2Result.add(CarreraParalela.minunum(list2).get(1));
+        list2Result.add(CarreraParalela.validateMultipleOfTwo(list2));
+        list2Result.add(CarreraParalela.validateDivOfTwo(list2));
+        list2Result.add(CarreraParalela.validateEvenSize(list2));
     }
+
     @Override
     public String toString() {
-        return "List 1: " +"max=" + max1 +", min=" + min1 +", count=" + count1 +", isMaxMultipleOfTwo=" + isMax1MultipleOfTwo + ", isMaxDivisorOfTwo=" + isMax1DivisorOfTwo +
-                ", isCountEven=" + isCount1Even + "\n"+
-                "List 2: " +"max=" + max2 +", min=" + min2 +", count=" + count2 +", isMaxMultipleOfTwo=" + isMax2MultipleOfTwo + ", isMaxDivisorOfTwo=" + isMax2DivisorOfTwo +
-                ", isCountEven=" + isCount2Even;
+        String[] labels = {"max: ", "min: ", "Size: ", "MultipleOfTwo: ", "DivOfTwo: ", "EvenSize: "};
+
+        String list1Str = String.join("", list1Result.stream().map((value)->"  "+labels[list1Result.indexOf(value)]+value+"\n").toArray(String[]::new));
+        String list2Str = String.join("", list2Result.stream().map((value)->"  "+labels[list2Result.indexOf(value)]+value+"\n").toArray(String[]::new));
+
+        return "List 1:\n"+list1Str+"\nList 2:\n"+list2Str;
+
     }
 }
