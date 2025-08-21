fbba43d41327865e6bf4109c0a13e9855bcb7ae4
DaniAleRoB <daniel.rodriguez-b@mail.escuelaing.edu.co>
2025-08-21 11:20:26 -0500

    Funcion encontrar numero maximo terminada

--- Pareja_rodriguez_martinez/reto2/CarreraParalela.java
+++ Pareja_rodriguez_martinez/reto2/CarreraParalela.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class CarreraParalela {
 
-    public int findMinimum(List<Integer> listOfIntegers ){
-        return listOfIntegers.stream().min(Integer::compare).get();
+    public int findMax(List<Integer> listOfIntegers ){
+        return listOfIntegers.stream().max(Integer::compare).get();
     }
 
 }
