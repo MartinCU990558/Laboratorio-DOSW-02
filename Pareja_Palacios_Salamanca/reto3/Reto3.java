af50a9be33838da0bbcd5f5eba8fccce4bdb3a1c
komi_yes <daniel.palacios-m@mail.escuelaing.edu.co>
2025-08-21 14:55:16 -0500

    feature: Creacion del metodo del eco de la cueva  con StringBuilder

--- Pareja_Palacios_Salamanca/reto3/Reto3.java
+++ Pareja_Palacios_Salamanca/reto3/Reto3.java
@@ -1,6 +1,11 @@
 public class Reto3{
     public static void main(String[] args) {
-
+        eco(hola);
     }
 
+    public void eco(String mensaje){
+        StringBuilder string = new StringBuilder();
+        string.append(mensaje).append(" ").append(mensaje).append(" ").append(mensaje);
+        System.out.println(string);
+    }
 }
