4fbf2d8c22b08feeb1d23369bd16123a0acfabbd
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-08-21 17:53:18 -0500

    estudiante A commit numero 2

--- reto3/Reto3.java
+++ reto3/Reto3.java
@@ -2,9 +2,11 @@
 package reto3;
 
 class Reto3 {
-    public static String repetirMensaje(String mensaje) {
-        StringBuilder sb = new StringBuilder();
-        java.util.stream.IntStream.range(0, 3).forEach(i -> {if (i > 0) sb.append(" ");sb.append(mensaje);});
-        return sb.toString();
+    public static String repetirTresVeces(String mensaje) {
+        return IntStream.range(0, 3).mapToObj(i -> mensaje).reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append).toString();
     }
+    public static String repetirEInvertir(String mensaje) {
+        return invertir(repetirTresVeces(mensaje));
+    }
+
 }
