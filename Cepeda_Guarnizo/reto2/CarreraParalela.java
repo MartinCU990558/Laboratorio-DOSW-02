8ded48c82fbab11fd0a4e249fd554356e2d7861c
MAGG0059 <manuel.guarnizo-g@mail.escuelaing.edu.co>
2025-08-20 19:49:17 -0500

    feature: lista esPar

--- Cepeda_Guarnizo/reto2/CarreraParalela.java
+++ Cepeda_Guarnizo/reto2/CarreraParalela.java
@@ -1,12 +1,13 @@
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-public class CarreraParalela{
 
-    public static void main(String[] args){
+public class CarreraParalela {
+
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String entrada = scan.nextLine().trim();
-       
+
         if (entrada.startsWith("{") && entrada.endsWith("}")) {
                 List<Integer> numeros = Arrays.stream(
                         entrada.substring(1, entrada.length() - 1).split(","))
@@ -18,10 +19,12 @@ public class CarreraParalela{
                     int min = lista.stream().min(Integer::compareTo).get();
                     int max = lista.stream().max(Integer::compareTo).get();
                     String esMultiplo =(max%2 ==0)?"es multiplo de 2":"No es multiplo de 2";
-                    return min + " " + max +" " +esMultiplo+" "+lista.size();
+                    String esPar = (lista.size() % 2 == 0) ? "La cantidad es par" : "La cantidad es impar";
+
+                    return min + " " + max +" " +esMultiplo+" "+lista.size() +" " +esPar;
                 };
 
-                System.out.println(resultado.apply(numeros));
-            }
+            System.out.println(resultado.apply(numeros));
         }
     }
+}
