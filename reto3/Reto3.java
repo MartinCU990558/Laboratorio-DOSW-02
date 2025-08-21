ce8c33a6021db8679d3c279d26d012b66e678abe
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-08-21 17:49:43 -0500

    Estudiante b segundo commit

--- reto3/Reto3.java
+++ reto3/Reto3.java
@@ -1,8 +1,12 @@
 package reto3;
 
 class Reto3 {
-    public static String invertir() {
-        return mensaje -> new StringBuffer(mensaje).reverse().toString();
+    public static String invertir(String mensaje) {
+        return new StringBuffer(mensaje).reverse().chars().mapToObj(c -> (char)c).map(String::valueOf).collect(Collectors.joining());
+    }
+
+    public static String repetirEInvertir(String mensaje) {
+        return invertir(repetirTresVeces(mensaje));
     }
 
 }
