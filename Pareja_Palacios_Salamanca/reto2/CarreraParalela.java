2139a13426689a7623d38d53be8a91834cdda4d5
David Salamanca <david.salamanca-a@mail.escuelaing.edu.co>
2025-08-21 15:41:16 -0500

    Implementación mínimo de lista y cantidad de elementos ingresados

--- Pareja_Palacios_Salamanca/reto2/CarreraParalela.java
+++ Pareja_Palacios_Salamanca/reto2/CarreraParalela.java
@@ -1,5 +1,24 @@
+import java.util.List;
+import java.util.Optional;
+import java.util.Arrays;
+
 public class CarreraParalela{
+
+    public static List<Integer> numeros = Arrays.asList(5, 2, 9, 1, 7, 12, 4);
+
     public static void main(String[] args) {
-        a;
+        calcularMinimoYCantidad();
+    }
+
+    private static void calcularMinimoYCantidad(){
+        Optional<Integer> minimo = numeros.stream()
+                .min((a, b) -> a - b);
+        long cantidad = numeros.stream().count();
+        if (minimo.isPresent()) {
+            System.out.println("El número más pequeño es: " + minimo.get());
+            System.out.println("La cantidad de números ingresados es: " + cantidad);
+        } else {
+            System.out.println("La lista está vacía");
+        }
     }
 }
