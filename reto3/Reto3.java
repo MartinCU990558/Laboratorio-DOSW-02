053d93b4b9dbf1b80f8ace529717facc57120a0f
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-08-21 17:05:46 -0500

    reto 3 estudianteA primero

---
+++ reto3/Reto3.java
@@ -0,0 +1,13 @@
+package reto3;
+
+class Reto3 {
+    public static void main(String[] args) {}
+
+    public static String repetirMensaje(String mensaje) {
+        StringBuilder sb = new StringBuilder();
+        java.util.stream.IntStream.range(0, 3).forEach(i -> {if (i > 0) sb.append(" ");sb.append(mensaje);});
+        return sb.toString();
+    }
+
+}
+
