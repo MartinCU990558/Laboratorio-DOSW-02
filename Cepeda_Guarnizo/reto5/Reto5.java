ff458070a2e18aad6bf8a8daec5754fa2f01aa97
carolina-cepeda <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-08-21 15:10:50 -0500

    feature: esqueleto reto5

--- Cepeda_Guarnizo/reto5/Reto5.java
+++ Cepeda_Guarnizo/reto5/Reto5.java
@@ -1,5 +1,27 @@
 package Cepeda_Guarnizo.reto5;
 
+import java.util.*;
+
 public class Reto5 {
-    
+
+    public static Set<Integer> hashSet(int cantidad, int min, int max) {
+        return new HashSet<>();
+    }
+
+    public static Set<Integer> treeSet(int cantidad, int min, int max) {
+        return new TreeSet<>();
+    }
+
+    public static SortedSet<Integer> unionOrdenada(Set<Integer> a, Set<Integer> b) {
+
+        return new TreeSet<>();
+    }
+
+    public static void imprimir(SortedSet<Integer> arena) {
+        arena.forEach(n -> System.out.println("EN ARENA: " + n));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto listo. Implementa métodos en las ramas.");
+    }
 }
