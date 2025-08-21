1a0b1ec2a061ca55ce4a265bb2f1b3184575bed4
Elizabeth Correa <juan.contreras-p@mail.escuelaing.edu.co>
2025-08-21 17:12:29 -0500

    reto 3 estudianteA primero

--- reto3/Reto3.java
+++ reto3/Reto3.java
@@ -3,6 +3,12 @@ package reto3;
 class Reto3 {
     public static void main(String[] args) {}
 
+    /**
+     *
+     *
+     * @param mensaje
+     * @return
+     */
     public static String repetirMensaje(String mensaje) {
         StringBuilder sb = new StringBuilder();
         java.util.stream.IntStream.range(0, 3).forEach(i -> {if (i > 0) sb.append(" ");sb.append(mensaje);});
