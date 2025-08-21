b9fac54d9ef95d5ef630b82db657a244ee8b2b30
NikoMAR3 <nikolas.martinezrive@mail.escuelaing.edu.co>
2025-08-21 10:42:54 -0500

    Se a;ade la funcion del minimo de una lista

--- Pareja_rodriguez_martinez/reto2/CarreraParalela.java
+++ Pareja_rodriguez_martinez/reto2/CarreraParalela.java
@@ -1,5 +1,11 @@
 package Pareja_rodriguez_martinez.reto2;
 
+import java.util.List;
+
 public class CarreraParalela {
-    
+
+    public static Integer calculateMinimum(List<Integer> listOfNumbers){
+        return listOfNumbers.stream().min(Integer::compare).get();
+    }
 }
+
