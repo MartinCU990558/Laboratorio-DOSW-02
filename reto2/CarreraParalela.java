45970366583653013414c4f4b88051a2371fc3b9
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-08-20 21:55:36 -0500

    Primer choque estudiante B

--- reto2/CarreraParalela.java
+++ reto2/CarreraParalela.java
@@ -9,4 +9,21 @@ public class CarreraParalela {
     public static int getMax(List<Integer> l) {
         return (l.stream().max(Integer::compare).get());
     }
+
+    public void crash(List<Integer> l) {
+        new Result(getMax(l), getMinAndAmount(l).get(0),getMinAndAmount(l).get(1));
+    }
+
+}
+
+class Result {
+    private int max;
+    private int min;
+    private int count;
+
+    public Result(int max, int min, int count) {
+        this.max = max;
+        this.min = min;
+        this.count = count;
+    }
 }
