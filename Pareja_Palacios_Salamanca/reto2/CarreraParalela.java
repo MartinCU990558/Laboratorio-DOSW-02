38b8ce2f176974321b5efc529a4a90427a45d408
komi_yes <daniel.palacios-m@mail.escuelaing.edu.co>
2025-08-21 14:40:07 -0500

    feature: Creacion de la funcion que verifica el numero mas grande de un arreglo

--- Pareja_Palacios_Salamanca/reto2/CarreraParalela.java
+++ Pareja_Palacios_Salamanca/reto2/CarreraParalela.java
@@ -1,5 +1,21 @@
+import java.util.List;
+import java.util.Optional;
+import java.util.Arrays;
+import java.util.Comparator;
 public class CarreraParalela{
-    public static void main(String[] args) {
 
+    public static List<Integer> numeros = Arrays.asList(5, 2, 9, 1, 7, 12, 4);
+    public static void main(String[] args) {
+        numero();
+    }
+
+    private static void numero(){
+        Optional<Integer> maximo = numeros.stream().max((a, b) -> a - b);
+
+        if (maximo.isPresent()) {
+            System.out.println("El número más grande es: " + maximo.get());
+        } else {
+            System.out.println("La lista está vacía");
+        }
     }
 }
