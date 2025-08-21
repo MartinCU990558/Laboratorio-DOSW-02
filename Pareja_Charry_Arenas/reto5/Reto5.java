7736d7863c8a7cd043c36fafab799ef35b9dd4ad
Marlio Charry <marlio.charry@sainapsis.com>
2025-08-20 22:09:34 -0500

    reto 5 hard merge

--- Pareja_Charry_Arenas/reto5/Reto5.java
+++ Pareja_Charry_Arenas/reto5/Reto5.java
@@ -1,12 +1,11 @@
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class BatallaConjuntosA {
+public class BatallaConjuntosFinal {
 
     private static final Random RNG = new Random();
 
@@ -20,12 +19,36 @@ public class BatallaConjuntosA {
                 .collect(Collectors.toCollection(HashSet::new));
     }
 
+    public static Set<Integer> crearTreeSetFiltrandoMultiplosDe5(int cantidad, int maxExclusive) {
+        TreeSet<Integer> base = new TreeSet<>();
+        while (base.size() < cantidad) {
+            base.add(RNG.nextInt(maxExclusive));
+        }
+        return base.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
     public static Set<Integer> unirColeccionesOrdenadas(Set<Integer> hashSetA, Set<Integer> treeSetB) {
-        TreeSet<Integer> unidos = new TreeSet<>(hashSetA);
-        unidos.addAll(treeSetB);
+        Set<Integer> unidos = Stream.concat(hashSetA.stream(), treeSetB.stream())
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        unidos.forEach(n -> System.out.println("Número en arena: " + n));
+
         return unidos;
     }
-    public static void imprimirArena(Set<Integer> numeros) {
-        numeros.forEach(n -> System.out.println("Número en arena: " + n));
+
+    public static Set<Integer> desdeArrayHashSet(int... valores) {
+        Set<Integer> s = new HashSet<>();
+        for (int v : valores) s.add(v);
+        return s.stream().filter(n -> n % 3 != 0)
+                .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    public static Set<Integer> desdeArrayTreeSet(int... valores) {
+        TreeSet<Integer> s = new TreeSet<>();
+        for (int v : valores) s.add(v);
+        return s.stream().filter(n -> n % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new));
     }
 }
