e4228482fce5177843cfb589e763f8665cc8b22b
David Salamanca <david.salamanca-a@mail.escuelaing.edu.co>
2025-08-21 17:36:30 -0500

    feature: Implementación buffer reto 3

---
+++ Pareja_Palacios_Salamanca/reto3/Reto3.java
@@ -0,0 +1,11 @@
+public class Reto3{
+    public static void main(String[] args) {
+        eco("hola");
+    }
+
+    public static void eco(String mensaje){
+        StringBuffer string = new StringBuffer(mensaje);
+        string.reverse().toString();
+        System.out.println(string);
+    }
+}
