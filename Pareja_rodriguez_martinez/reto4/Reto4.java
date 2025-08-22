ca08e9e37fdba26bcf3a239c431edf711fd6c37f
DaniAleRoB <daniel.rodriguez-b@mail.escuelaing.edu.co>
2025-08-21 23:36:46 -0500

    Se soluciono el primer choque

--- Pareja_rodriguez_martinez/reto4/Reto4.java
+++ Pareja_rodriguez_martinez/reto4/Reto4.java
@@ -22,4 +22,24 @@ public class Reto4 {
                         HashMap::new
                 ));
     }
+    public static HashMap<String,Integer> saveInHashMap(List<Object[]> listToSave){
+        return listToSave.stream()
+                .collect(Collectors.toMap(
+                        arr -> (String) arr[0],
+                        arr -> (Integer) arr[1],
+                        (v1, v2) -> v1,
+                        HashMap::new
+                ));
+    }
+
+    public static Map<String, Integer> sortHashTable(Hashtable<String, Integer> tabla) {
+        return tabla.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (v1, v2) -> v1,
+                        LinkedHashMap::new
+                ));
+    }
 }
