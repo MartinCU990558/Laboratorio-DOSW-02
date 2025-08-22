f86be5b329dd2b0ff7b90f28dbac1c81c38f22dc
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-08-21 21:31:23 -0500

    estudiante a commit 2 edit

--- reto5/Reto5.java
+++ reto5/Reto5.java
@@ -5,7 +5,7 @@ import java.util.stream.*;
 
 
 public class Reto5 {
-    public static void almacenarNumeros2() {
+    public static Set<Integer> almacenarNumeros2() {
         HashSet<Integer> numeros = new HashSet<>();
         Random rand = new Random();
         for (int i = 0; i < 20; i++) {
@@ -19,10 +19,13 @@ public class Reto5 {
 
         System.out.println("\n\nNúmeros después de eliminar múltiplos de 3:");
         filtrados.forEach(n -> System.out.print(n + " "));
+        return filtrados;
     }
 
-    public static void crash(Set<Integer> setA, Set<Integer> setB){
+    public static void crash(){
         TreeSet<Integer> union = new TreeSet<>();
+        Set<Integer> setA = almacenarA();
+        Set<Integer> setB = almacenarB();
         union.addAll(setA);
         union.addAll(setB);
         System.out.println("Resultado: ");
