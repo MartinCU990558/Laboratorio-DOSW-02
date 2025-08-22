469dd0c5578ce8ce543d00d6963286c0bdf7a339
komi_yes <danielpalacios04@gmail.com>
2025-08-21 21:20:41 -0500

    feature: asignacion de valores al mapa dentro del  metodo sin repeticioin de llaves con Hashmap

--- Pareja_Palacios_Salamanca/reto4/Reto4.java
+++ Pareja_Palacios_Salamanca/reto4/Reto4.java
@@ -1,12 +1,16 @@
 import java.util.HashMap;
 
 public class Reto4 {
-    public static HashMap mapa;
+    public static HashMap<String,Integer> mapa;
     public static void main(String[] args) {
-        mapa  = new HashMap();
-        mapa.put("a",1);
-    }
-    public static void map(String clave, Integer valor){
+        mapa  = new HashMap<>();
+        map("a",1);
 
+    }
+
+    public static void map(String clave, Integer valor){
+        if(mapa.containsKey(clave)){
+            mapa.put(clave, valor);
+        }
     }
 }
