cc7e95d0295924bf0de06b61b640add3cae9849e
David Salamanca <david.salamanca-a@mail.escuelaing.edu.co>
2025-08-21 21:44:06 -0500

    feature: Validación de implementación hashtable

--- Pareja_Palacios_Salamanca/reto4/Reto4.java
+++ Pareja_Palacios_Salamanca/reto4/Reto4.java
@@ -3,12 +3,14 @@ import java.util.Hashtable;
 public class Reto4 {
     public static Hashtable<String,Integer> mapa;
     public static void main(String[] args) {
+
         mapa  = new Hashtable<>();
         map("a",1);
 
     }
 
     public static void map(String clave, Integer valor){
+
         if(mapa.containsKey(clave)){
             mapa.put(clave, valor);
         }
