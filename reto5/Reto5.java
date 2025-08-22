f990db07eeb34f75589c9570201993e9b9723f4e
juancontrerasp <juan.contreras-p@mail.escuelaing.edu.co>
2025-08-21 23:17:45 -0500

    Update Reto5.java

--- reto5/Reto5.java
+++ reto5/Reto5.java
@@ -3,21 +3,24 @@ package reto5;
 import java.util.*;
 import java.util.stream.*;
 
-
 public class Reto5 {
-    public static Set<Integer> almacenarA() {
-        HashSet<Integer> numeros = new HashSet<>();
+
+    public static Set<Integer> almacenarB() {
+        TreeSet<Integer> numeros = new TreeSet<>();
         Random rand = new Random();
+
         for (int i = 0; i < 20; i++) {
             numeros.add(rand.nextInt(100) + 1);
         }
 
-        System.out.println("Números generados:");
+        System.out.println("Números generados en orden ascendente:");
         numeros.forEach(n -> System.out.print(n + " "));
 
-        Set<Integer> filtrados = numeros.stream().filter(n -> n % 3 != 0).collect(Collectors.toSet());
+        Set<Integer> filtrados = numeros.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new));
 
-        System.out.println("\n\nNúmeros después de eliminar múltiplos de 3:");
+        System.out.println("\n\nNúmeros después de eliminar múltiplos de 5:");
         filtrados.forEach(n -> System.out.print(n + " "));
         return filtrados;
     }
@@ -31,4 +34,5 @@ public class Reto5 {
         System.out.println("Resultado: ");
         union.forEach(n -> System.out.print(n + " "));
     }
+
 }
