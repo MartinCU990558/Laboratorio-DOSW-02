d68474d28c2e7a60a0609413e333d06eb93c67a5
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-08-21 21:03:03 -0500

    commit 1 estudiante a edit

--- reto5/Reto5.java
+++ reto5/Reto5.java
@@ -5,12 +5,19 @@ import java.util.stream.*;
 
 
 public class Reto5 {
-    public static void storeA(){
+    public static void almacenarNumeros2() {
         HashSet<Integer> numeros = new HashSet<>();
         Random rand = new Random();
-        for (int i = 0; i < 20; i++){
-            numeros.add(rand.nextInt(100)+1);
+        for (int i = 0; i < 20; i++) {
+            numeros.add(rand.nextInt(100) + 1);
         }
-        Set<Integer> filtrados = numeros.stream().filter(n-> n % 3 != 0).collect(Collectors.toSet());
+
+        System.out.println("Números generados:");
+        numeros.forEach(n -> System.out.print(n + " "));
+
+        Set<Integer> filtrados = numeros.stream().filter(n -> n % 3 != 0).collect(Collectors.toSet());
+
+        System.out.println("\n\nNúmeros después de eliminar múltiplos de 3:");
+        filtrados.forEach(n -> System.out.print(n + " "));
     }
 }
