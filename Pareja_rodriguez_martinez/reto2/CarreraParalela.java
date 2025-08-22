12c30b7a847e736d4a70b6e42179ae67f3a67073
DaniAleRoB <daniel.rodriguez-b@mail.escuelaing.edu.co>
2025-08-21 19:55:48 -0500

    añadir funciones de paridad y multiplicidad terminadas

--- Pareja_rodriguez_martinez/reto2/CarreraParalela.java
+++ Pareja_rodriguez_martinez/reto2/CarreraParalela.java
@@ -16,4 +16,17 @@ public class CarreraParalela {
         return listOfIntegers;
     }
 
+<<<<<<< Updated upstream
+=======
+    public static List<Integer> unifyResults(Integer min, Integer max){
+        return new ArrayList<>(Arrays.asList(min, max));
+    }
+
+    public static boolean isEven(Integer number){
+        return (number % 2 == 0) ? true:false;
+    }
+    public static boolean isTheListLongEven(List<Integer> lista) {
+        return lista.size() % 2 == 0;
+    }
+>>>>>>> Stashed changes
 }
