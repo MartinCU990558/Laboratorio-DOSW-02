c07371df79f5e3f0df7c7960f447d23ddf95d59a
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-08-21 21:27:57 -0500

    estudiante b commit 2 edit

--- reto5/reto5.java
+++ reto5/reto5.java
@@ -5,7 +5,7 @@ import java.util.stream.*;
 
 public class Reto5 {
 
-    public static void almacenarNumeros() {
+    public static Set<Integer> almacenarB() {
         TreeSet<Integer> numeros = new TreeSet<>();
         Random rand = new Random();
 
@@ -22,10 +22,13 @@ public class Reto5 {
 
         System.out.println("\n\nNúmeros después de eliminar múltiplos de 5:");
         filtrados.forEach(n -> System.out.print(n + " "));
+        return filtrados;
     }
 
-    public static void crash(Set<Integer> setA, Set<Integer> setB){
+    public static void crash(){
         TreeSet<Integer> union = new TreeSet<>();
+        Set<Integer> setA = almacenarA();
+        Set<Integer> setB = almacenarB();
         union.addAll(setA);
         union.addAll(setB);
         System.out.println("Resultado: ");
