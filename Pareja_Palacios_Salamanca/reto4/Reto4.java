1795e7068645dbb6bb063fb0769b23fb99d33881
komi_yes <danielpalacios04@gmail.com>
2025-08-21 21:25:19 -0500

    feature: correccion valores HashMap

--- Pareja_Palacios_Salamanca/reto4/Reto4.java
+++ Pareja_Palacios_Salamanca/reto4/Reto4.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 public class Reto4 {
-    public static HashMap mapa;
+    public static HashMap<String, Integer> mapa;
     public static void main(String[] args) {
         mapa  = new HashMap();
         map("a",1);
