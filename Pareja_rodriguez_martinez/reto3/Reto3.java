9328cf4f939d552db2b29d267ec107729d3f3089
DaniAleRoB <daniel.rodriguez-b@mail.escuelaing.edu.co>
2025-08-21 22:18:44 -0500

    Funcion para combinar las funciones repetir tres veces e invertir terminadas

--- Pareja_rodriguez_martinez/reto3/Reto3.java
+++ Pareja_rodriguez_martinez/reto3/Reto3.java
@@ -1,11 +1,22 @@
 package Pareja_rodriguez_martinez.reto3;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Reto3 {
 
     public static String invertString(String str) {
         return new StringBuffer(str).reverse().toString();
     }
+    public static String printWithBuilder(String str) {
+        return " ";
+    }
 
+    public static String combineTransformations(String message) {
+        return invertString(printWithBuilder(message));
+    }
+    public static void printMessage(String msg){
+        Function<String,String> transformar = Reto3 :: combineTransformations;
+        System.out.println(transformar.apply(msg));
+    }
 }
