507329c15b2f774a95553f32d48eaab5987be89d
juancontrerasp <juan.contreras-p@mail.escuelaing.edu.co>
2025-08-21 23:21:58 -0500

    Update Reto5.java
    manual cuz ultra bug

--- reto5/Reto5.java
+++ reto5/Reto5.java
@@ -22,6 +22,26 @@ public class Reto5 {
         return filtrados;
     }
 
+        public static Set<Integer> almacenarB() {
+        TreeSet<Integer> numeros = new TreeSet<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            numeros.add(rand.nextInt(100) + 1);
+        }
+
+        System.out.println("Números generados en orden ascendente:");
+        numeros.forEach(n -> System.out.print(n + " "));
+
+        Set<Integer> filtrados = numeros.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        System.out.println("\n\nNúmeros después de eliminar múltiplos de 5:");
+        filtrados.forEach(n -> System.out.print(n + " "));
+        return filtrados;
+    }
+
     public static void crash(){
         TreeSet<Integer> union = new TreeSet<>();
         Set<Integer> setA = almacenarA();
