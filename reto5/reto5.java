436891ae642e9479c2b6d2604596df08f90f0a05
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-08-21 20:52:20 -0500

    estudiante B commit número 1

---
+++ reto5/reto5.java
@@ -0,0 +1,27 @@
+package reto5;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class EstudianteB {
+
+    public static void almacenarNumeros() {
+        TreeSet<Integer> numeros = new TreeSet<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            numeros.add(rand.nextInt(100) + 1);
+        }
+
+        System.out.println("Números generados en orden ascendente:");
+        numeros.forEach(n -> System.out.print(n + " "));
+
+        Set<Integer> filtrados = numeros.stream()
+                .filter(n -> n % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new));
+
+        System.out.println("\n\nNúmeros después de eliminar múltiplos de 5:");
+        filtrados.forEach(n -> System.out.print(n + " "));
+    }
+
+}
