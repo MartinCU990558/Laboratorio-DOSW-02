a7e28d39af97f71d9240b3ae1c3633efe76b0aaa
komi_yes <danielpalacios04@gmail.com>
2025-08-21 22:29:25 -0500

    feature: creacion de metodo de unificacion de mapas

--- Pareja_Palacios_Salamanca/reto4/Reto4.java
+++ Pareja_Palacios_Salamanca/reto4/Reto4.java
@@ -1,17 +1,46 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class Reto4 {
-    public static HashMap<String, Integer> mapa;
-    public static void main(String[] args) {
-        mapa  = new HashMap();
-        map("a",1);
 
+    public static HashMap<String, Integer> mapa;
+    public static Hashtable<String, Integer> table;
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> mapa = createHashMap();
+
+        Hashtable<String, Integer> table = new Hashtable<>();
+        table.put("plata", 8);
+        table.put("rubí", 4);
+        table.put("oro", 12);
+        table.put("esmeralda", 6);
+
+        unificar(table, mapa);
     }
 
-    public static void map(String clave, Integer valor){
-        if(mapa.containsKey(clave)){
-            mapa.put(clave, valor);
+    public static HashMap<String,Integer> createHashMap(){
+        HashMap<String, Integer> mapa = new HashMap<>();
+        mapa.put("oro", 5);
+        mapa.put("plata", 3);
+        mapa.put("diamante", 10);
+        return mapa;
+    }
 
+    public static void unificar(Hashtable<String, Integer> table, HashMap<String, Integer> mapa){
+        HashMap<String,Integer> resultado = new HashMap<>();
+        for (String key : table.keySet()) {
+            resultado.put(key, table.get(key));
+            if (mapa.keySet().contains(key)){
+                mapa.remove(key);
+            }
         }
+        for (String key : mapa.keySet()) {
+            if (!resultado.containsKey(key)){
+                resultado.put(key, mapa.get(key));
+            }
+        }
+        resultado.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByKey(String.CASE_INSENSITIVE_ORDER))
+                .map(entry -> String.format("Clave: %s | Valor: %d", entry.getKey().toUpperCase(), entry.getValue()))
+                .forEach(System.out::println);
     }
 }
