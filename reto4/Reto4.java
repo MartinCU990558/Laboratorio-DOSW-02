2290d2954c33348d28e1d28a18cbfa043bd7e2fc
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-08-21 20:21:03 -0500

    combinados

--- reto4/Reto4.java
+++ reto4/Reto4.java
@@ -5,11 +5,43 @@ import java.util.stream.Stream;
 
 public class Reto4 {
 
+    static class KV {
+        private final String key;
+        private final Integer value;
+
+        public KV(String key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return this.key;
+        }
+
+        public Integer getValue() {
+            return this.value;
+        }
+    }
+
     public static Hashtable<String, Integer> buildHashtableIgnoringDuplicatesFirst(List<KV> pairs) {
         Hashtable<String, Integer> table = new Hashtable<>();
         pairs.forEach(p -> table.putIfAbsent(p.getKey(), p.getValue()));
         return table;
     }
-}
 
+    public static HashMap<String, Integer> buildHashMapIgnoringDuplicatesFirst(List<KV> pairs) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        pairs.forEach(p -> map.putIfAbsent(p.getKey(), p.getValue()));
+        return map;
+    }
+
+    public static Map<String, Integer> combinarMapas(HashMap<String, Integer> mapaRapido, Hashtable<String, Integer> mapaBlindado) {
+        Map<String, Integer> merged = new HashMap<>(mapaRapido);
+        mapaBlindado.forEach((clave, valor) -> merged.put(clave, valor));
+
+        return merged;
+    }
+
+}
 //
