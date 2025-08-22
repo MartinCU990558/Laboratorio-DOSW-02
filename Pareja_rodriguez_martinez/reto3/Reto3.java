e097d9ed9461833d47d5b166ec5320f00264e656
NikoMAR3 <nikolas.martinezrive@mail.escuelaing.edu.co>
2025-08-21 22:18:36 -0500

    Funciones de repeticion y combinacion listas

--- Pareja_rodriguez_martinez/reto3/Reto3.java
+++ Pareja_rodriguez_martinez/reto3/Reto3.java
@@ -1,11 +1,26 @@
 package Pareja_rodriguez_martinez.reto3;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Reto3 {
 
-    public int findMinimum(List<Integer> listOfIntegers ){
-        return listOfIntegers.stream().min(Integer::compare).get();
+    public static void printMessage(String msg){
+        Function<String,String> transformar = Reto3 :: combineTransformations;
+        System.out.println(transformar.apply(msg));
+    }
+
+    public static String printWithBuilder(String msg){
+        return Stream.generate(()->msg).limit(3).collect(Collectors.joining(" "));
+    }
+
+    public static void combineTransformations(String msg){
+        return invertString(printWithBuilder(msg));
+    }
+    public static String invertString(){
+        return "x"
     }
 
 }
