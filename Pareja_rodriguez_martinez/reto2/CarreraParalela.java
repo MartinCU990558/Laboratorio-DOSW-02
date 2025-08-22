b60cde5bcd9a8c20b9f29ae5c0c16b4866b34233
DaniAleRoB <daniel.rodriguez-b@mail.escuelaing.edu.co>
2025-08-21 19:27:04 -0500

    Funcion unificadora del minimo y maximo completa

--- Pareja_rodriguez_martinez/reto2/CarreraParalela.java
+++ Pareja_rodriguez_martinez/reto2/CarreraParalela.java
@@ -1,11 +1,19 @@
 package Pareja_rodriguez_martinez.reto2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarreraParalela {
 
     public int findMax(List<Integer> listOfIntegers ){
         return listOfIntegers.stream().max(Integer::compare).get();
+    }
+
+    public List<Integer> unifyResults(Integer min, Integer max){
+        List<Integer> listOfIntegers = new ArrayList<>();
+        listOfIntegers.add(min);
+        listOfIntegers.add(max);
+        return listOfIntegers;
     }
 
 }
