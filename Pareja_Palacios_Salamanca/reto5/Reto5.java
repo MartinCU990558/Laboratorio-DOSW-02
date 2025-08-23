a0d734c8edd039e5e636473b990cd0cea6e7aca3
komi_yes <danielpalacios04@gmail.com>
2025-08-23 11:17:14 -0500

    feature: esqueleto de la solucion reto #5

--- Pareja_Palacios_Salamanca/reto5/Reto5.java
+++ Pareja_Palacios_Salamanca/reto5/Reto5.java
@@ -1,5 +1,46 @@
 public class Reto5{
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String inputHashSet = scanner.nextLine();
+
+        String inputTreeSet = scanner.nextLine();
+
+        scanner.close();
+
+        Set<Integer> conjuntoA = extraerNumeros(inputHashSet);
+        Set<Integer> conjuntoB = extraerNumeros(inputTreeSet);
+
+        Set<Integer> resultadoA = procesarHashSet(conjuntoA);
+        Set<Integer> resultadoB = procesarTreeSet(conjuntoB);
+
+        Set<Integer> union = unirConjuntos(resultadoA, resultadoB);
+
+        mostrarResultado(union);
+    }
+
+    public static Set<Integer> extraerNumeros(String input) {
+
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(input);
+
+        Set<Integer> numeros = new HashSet<>();
+        while (matcher.find()) {
+            numeros.add(Integer.parseInt(matcher.group()));
+        }
+        return numeros;
+    }
+
+    public static Set<Integer> procesarHashSet(Set<Integer> conjunto) {
+    }
+
+    public static Set<Integer> procesarTreeSet(Set<Integer> conjunto) {
+
+    }
+    public static Set<Integer> unirConjuntos(Set<Integer> conjuntoA, Set<Integer> conjuntoB) {
+    }
+
+    public static void mostrarResultado(Set<Integer> conjunto) {
+        conjunto.forEach(num -> System.out.println("Número en arena: " + num));
     }
 }
