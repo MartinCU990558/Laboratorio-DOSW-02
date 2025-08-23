b8f2c8e5b9bd1342a775266e98e8db77387e1c25
komi_yes <danielpalacios04@gmail.com>
2025-08-23 13:05:58 -0500

    feature: desarrollo funcion procesarTreeSet

--- Pareja_Palacios_Salamanca/reto5/Reto5.java
+++ Pareja_Palacios_Salamanca/reto5/Reto5.java
@@ -39,8 +39,11 @@ public class Reto5{
     }
 
     public static Set<Integer> procesarTreeSet(Set<Integer> conjunto) {
-
+        return conjunto.stream()
+                .filter(num -> num % 5 != 0)
+                .collect(Collectors.toCollection(TreeSet::new));
     }
+
     public static Set<Integer> unirConjuntos(Set<Integer> conjuntoA, Set<Integer> conjuntoB) {
     }
 
